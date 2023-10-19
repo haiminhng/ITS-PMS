cc797565d17aec9fec6b0c0ed32fb86ba0991c57
Hai Minh Nguyen <minhnguyen@outlook.de>
2023-10-19 07:46:53 +0200

    Update ParkplatzverwaltungContext.cs
    REMOVE SQL CONNECTION STRING

--- Models/Data/ParkplatzverwaltungContext.cs
+++ Models/Data/ParkplatzverwaltungContext.cs
@@ -26,7 +26,7 @@ public partial class ParkplatzverwaltungContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=130.61.200.85;Database=Parkplatzverwaltung;Persist Security Info=True;TrustServerCertificate=True;User ID=sa;Password=Minh@1996");
+        => optionsBuilder.UseSqlServer("SQL CONNECTIONSTRING ");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
